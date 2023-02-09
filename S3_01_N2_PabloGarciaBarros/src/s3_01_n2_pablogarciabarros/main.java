d43dcec60939d58f94b5ea96d427f5aea2420134
pgarcia86 <pgarciabarros86@gmail.com>
2023-02-09 01:23:22 +0100

    Primera subida Ejercicio 2

---
+++ S3_01_N2_PabloGarciaBarros/src/s3_01_n2_pablogarciabarros/main.java
@@ -0,0 +1,13 @@
+package s3_01_n2_pablogarciabarros;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola java");
+		System.out.println("Hola java");
+		System.out.println("Hola java");
+		
+	}
+
+}
